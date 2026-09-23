ab264412c3020bd450547871b124c3fb47f96ad2
VantaKruz <vedantparanjape9@gmail.cpm>
2026-09-23 16:46:07 +0530

    inital

---
+++ area.py
@@ -0,0 +1,9 @@
+radius= int(input("Enter radius of circle "))
+height= int(input("Enter height of triangle "))
+base=int(input("Enter base of triangle "))
+
+areaC=3.141*radius*radius
+areaT=0.5*height*base
+
+print("Area of Circle is:", areaC)
+print("Area of Traingle is", areaT)
