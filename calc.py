d5b294f07f257b626df4bbbc1cef143ed26c8230
VantaKruz <vedantparanjape9@gmail.cpm>
2026-09-23 17:20:28 +0530

    basic calculations

---
+++ calc.py
@@ -0,0 +1,8 @@
+n1=int(input("Enter a number "))
+n2=int(input("Enter another number "))
+
+print("Addition ", n1+n2)
+print("Subtraction ", n1-n2)
+print("Multiplication ", n1*n2)
+print("Division ", n1/n2)
+print("Remainder ",n1%n2)
